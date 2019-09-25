67f9b9d181099f87be86b0c41c1940e42dfed8ed
haohao809 <haohao809@126.com>
2019-09-25 21:12:14 +0800

    react事件处理

--- src/views/Login.js
+++ src/views/Login.js
@@ -4,8 +4,11 @@ class Login extends React.Component {
       return <div>
       <div>用户名:<input /></div>
       <div>密码:<input /></div>
-      <button style={{width:"100",height:"100"}}>登录</button>
+      <button style={{width:"100",height:"100"}} onClick={this.loginHandle}>登录</button>
   </div>
+    }
+    loginHandle() {
+      console.log('登录');
     }
 }
 export default Login
