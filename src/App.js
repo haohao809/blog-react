d678b442ca1f3dea20bdbed233cf62b2aa71e1e9
haohao809 <haohao809@126.com>
2019-10-05 12:12:49 +0800

    删除多余的路由

--- src/App.js
+++ src/App.js
@@ -11,40 +11,10 @@ function App() {
         <Header />
         <Route exact path="/" component={Login} />
         <Route path="/blog" component={Blog} />
-        <Route path="/topics" component={Topics} />
       </div>
     </Router>
   );
 }
-
-function Topic({ match }) {
-  return <h3>Requested Param: {match.params.id}</h3>;
-}
-
-function Topics({ match }) {
-  return (
-    <div>
-      <h2>Topics</h2>
-
-      <ul>
-        <li>
-          <Link to={`${match.url}/components`}>Components</Link>
-        </li>
-        <li>
-          <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
-        </li>
-      </ul>
-
-      <Route path={`${match.path}/:id`} component={Topic} />
-      <Route
-        exact
-        path={match.path}
-        render={() => <h3>Please select a topic.</h3>}
-      />
-    </div>
-  );
-}
-
 function Header() {
   return (
     <ul>
@@ -53,9 +23,6 @@ function Header() {
       </li>
       <li>
         <Link to="/blog">Blog</Link>
-      </li>
-      <li>
-        <Link to="/topics">Topics</Link>
       </li>
     </ul>
   );
