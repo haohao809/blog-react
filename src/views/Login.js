f299c5de1262fb6b4f75f033d15aafff14da36a8
anzhihao <anzhihao@jd.com>
2019-10-09 14:28:31 +0800

    注册用户名重复处理

--- src/views/Login.js
+++ src/views/Login.js
@@ -10,20 +10,25 @@ class Login extends React.Component {
       username: '',
       password: '',
       realname: '',
+      tips: '',
+      registername: '',
       dialogVisible3: false,
       dialogVisible2: false
     };
+    this.userlist = [];
     this.loginHandle = this.loginHandle.bind(this);
     this.registerHandle = this.registerHandle.bind(this);
     this.changeUsername = this.changeUsername.bind(this);
     this.changePassword = this.changePassword.bind(this);
     this.changeRealname = this.changeRealname.bind(this);
+    this.changeRegistername = this.changeRegistername.bind(this);
+    this.openRegister = this.openRegister.bind(this);
   }
 
   render() {
     return <div className="wrap">
-      <Button onClick={() => this.setState({ dialogVisible3: true })}>登录</Button>
-      <Button onClick={() => this.setState({ dialogVisible2: true })}>注册</Button>
+      <Button onClick={() => this.setState({ dialogVisible3: true, password: '', username: ''})}>登录</Button>
+      <Button onClick={this.openRegister}>注册</Button>
       <Dialog
         title="登录"
         visible={this.state.dialogVisible3}
@@ -53,13 +58,13 @@ class Login extends React.Component {
         <Dialog.Body>
           <Form model={this.state.form}>
             <Form.Item label="用户名" labelWidth="120">
-              <Input value={this.state.username} onChange={this.changeUsername}></Input>
+              <Input value={this.state.registername} onChange={this.changeRegistername} append={this.state.tips}></Input>
             </Form.Item>
             <Form.Item label="密码" labelWidth="120">
               <Input value={this.state.password} onChange={this.changePassword}></Input>
             </Form.Item>
             <Form.Item label="真实名字" labelWidth="120">
-              <Input value={this.state.realname} onChange={this.changeRealname}></Input>
+              <Input value={this.state.realname} onChange={this.changeRealname} ></Input>
             </Form.Item>
           </Form>
         </Dialog.Body>
@@ -109,10 +114,37 @@ class Login extends React.Component {
       realname: event
     })   
   }
+  changeRegistername(event) { 
+     if(this.userlist.includes(event)) {
+        this.setState({
+          tips: '用户名已存在'
+      })       
+     } else{
+        this.setState({
+          tips: ''
+      })       
+     }
+     this.setState({
+       registername: event
+     })
+  }
+ openRegister() {
+    this.setState({ 
+      dialogVisible2: true,
+      registername: '',
+      realname: '',
+      password: ''
+    })
+    this.checkuser();
+  }
   registerHandle() {
-    const username = this.state.username;
+    const username = this.state.registername;
     const password = this.state.password;
     const realname = this.state.realname;
+    if(this.state.tips) {
+       console.log('用户名已存在')
+       return false
+    }
     utils.postData('/api/users/register', { username: username, password: password, realname: realname })
       .then(data => {
         if (data.errorCode === 0) {
@@ -129,6 +161,18 @@ class Login extends React.Component {
       }
       )
       .catch(err => console.log('err', err))    
+  }
+  // 查询已有用户信息
+  checkuser() {
+    fetch("/api/users/checkuser")
+    .then((res) => res.json())
+    .then((userData) => {
+         if(userData.errorCode === 0) {
+            userData.data.forEach(item => {
+              this.userlist.push(item.username)
+           })
+         }
+    })
   }
 }
 export default Login
